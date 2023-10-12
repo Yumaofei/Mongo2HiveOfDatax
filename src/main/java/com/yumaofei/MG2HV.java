c8896c2c8f938bc906bfef602d8b171a2fa1d857
玉矛匪 <h1825127045@sina.com>
2023-10-12 14:26:07 +0800

    根据海豚的传参，切换使用构造方法

--- src/main/java/com/yumaofei/MG2HV.java
+++ src/main/java/com/yumaofei/MG2HV.java
@@ -7,7 +7,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.yumaofei.util.MongoDBUtil;
-import com.yumaofei.util.PropertiesUtil;
 import org.bson.Document;
 
 import java.util.*;
@@ -23,17 +22,16 @@ public class MG2HV {
     public static void main(String[] args) {
         //接收传递的参数
         String mongodbLocalhost = args[0];
-        int mongodbPort = Integer.parseInt(args[1]);
-        String mongodbUsrename = args[2];
-        String mongodbPassword = args[3];
-        String mongodbDatabase = args[4];
-        String mongodbCollection = args[5];
-        String fromSystem = args[6];
-        String tableType = args[7];
-        Boolean isPartition = Boolean.valueOf(args[8]);
-        String tableComment = args[9];
+        String mongodbUsrename = args[1];
+        String mongodbPassword = args[2];
+        String mongodbDatabase = args[3];
+        String mongodbCollection = args[4];
+        String fromSystem = args[5];
+        String tableType = args[6];
+        Boolean isPartition = Boolean.valueOf(args[7]);
+        String tableComment = args[8];
 
-        MongoDBUtil mongoDBUtil = new MongoDBUtil(mongodbLocalhost,mongodbPort,mongodbUsrename,mongodbPassword,mongodbDatabase);
+        MongoDBUtil mongoDBUtil = new MongoDBUtil(mongodbLocalhost,mongodbUsrename,mongodbPassword,mongodbDatabase);
         mongoDBUtil.certifyMongoClient();
 
         MongoDatabase db = mongoDBUtil.getDatabase(mongodbDatabase);
@@ -44,10 +42,10 @@ public class MG2HV {
 
         // 查询所有的数据
         int pageIndex = 1;
-        int pageSize = 1000;
+        int pageSize = 3000;
         BasicDBObject ship_confirm_date = new BasicDBObject();
         LinkedHashMap<String, String> data = new LinkedHashMap<String, String>();
-        while (pageIndex < 10) {
+        while (pageIndex < 15) {
             FindIterable findIterable = collection.find(ship_confirm_date).skip((pageIndex - 1) * pageSize * 10).sort(new BasicDBObject("_id",-1)).limit(pageSize);
             MongoCursor<Document> cur = findIterable.iterator();
             while (cur.hasNext()) {
